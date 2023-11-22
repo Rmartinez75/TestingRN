44f3c1aa610d3cdd21055f527f282323fc27b6ca
Rmartinez75 <rmartinez75@gmail.com>
2023-11-22 17:00:46 -0500

    First commit

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import CategoriesScreen from './screens/CategoriesScreen';
+import MealsOverveiwScreen from './screens/MealsOverveiwScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <StatusBar style='dark' />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name='MealsCategories' 
+          component={CategoriesScreen}
+        />
+        <Stack.Screen 
+          name='MealsOverview'
+          component={MealsOverveiwScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>    
+    </>
   );
 }
 
